5a251c1c165091be6079fb1c471ae5c04ee3eaf3
alex02r <alex.giochi02@gmail.com>
2023-11-20 13:45:39 +0100

    [UPGRADE]Add baseline variable for information

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Ask information at user
+let km = prompt('Inserisci quanti chilomentri vuoi percorrere: ');
+let eta = prompt('Inserisci la tua età: ');
+
+//define the price
+let prezzo = km * 0.21;
+
